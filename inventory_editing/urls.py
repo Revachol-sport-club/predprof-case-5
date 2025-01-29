705e0b557afddd36d830777c27e5c6cf91e281c5
Шабанов Андрей <124313432+Andaine09@users.noreply.github.com>
2025-01-29 14:48:24 +0300

    Add files via upload

--- inventory_editing/urls.py
+++ inventory_editing/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import inventory_editing, inventory_edit_instrument, inventory_purchase, inventory_report
+from .views import inventory_editing, inventory_edit_instrument, inventory_purchase, inventory_report, inventory_delete_instrument
 urlpatterns = [
     path('', inventory_editing, name="inventory_editing"),
     path('<int:id>', inventory_edit_instrument, name="edit_instrument"),
     path('purchase', inventory_purchase, name="inventory_purchase"),
-    path('report', inventory_report, name="inventory_report")
+    path('report', inventory_report, name="inventory_report"),
+    path('delete/<int:id>', inventory_delete_instrument, name="delete_instrument")
 ]
--- inventory_editing/views.py
+++ inventory_editing/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 import sqlite3
+import os
 from datetime import date
 def connect():
     connection = sqlite3.connect('./db.sqlite3', check_same_thread=False)
@@ -13,7 +14,7 @@ def inventory_editing(request):
     cursor.execute('SELECT * FROM Inventory')
     inventory = [list(i) for i in cursor.fetchall()]
     cursor.execute('SELECT * FROM auth_user')
-    users_usernames = ["Ничьё"] + [i[4] for i in [list(i) for i in cursor.fetchall()]]
+    users_usernames = ["Свободно"] + [i[4] for i in [list(i) for i in cursor.fetchall()]]
     cursor.close()
     try:
         index = inventory[-1][0] + 1
@@ -23,7 +24,7 @@ def inventory_editing(request):
         connection, cursor = connect()
         if request.POST.get("name", "")!="":
             for i in range(int(request.POST.get("quantity", ""))):
-                condition = "Используемый" if request.POST.get("belonging", "") != "Ничьё" and request.POST.get("condition", "") == "Новый" else request.POST.get("condition", "")
+                condition = "Используемый" if request.POST.get("belonging", "") != "Свободно" and request.POST.get("condition", "") == "Новый" else request.POST.get("condition", "")
                 cursor.execute('INSERT INTO Inventory (id, name, availability, condition, belonging) VALUES (?, ?, ?, ?, ?)', (index+i, request.POST.get("name",""), request.POST.get("availability",""), condition, request.POST.get("belonging",""),))
                 connection.commit()
             cursor.close()
@@ -73,6 +74,29 @@ def inventory_report(request):
             "instrument_names": [[i, len([j for j in inventory if j[1]==i])] for i in set([k[1] for k in inventory])],
             "users_instruments": [[i[4], [j[1] for j in inventory if j[-1]==i[4]]] for i in users],
     }
+    if request.method == "POST":
+        report = open("report "+str(date.today()) + ".txt", "w+")
+        report_text = f"Всего инструментов: {data['information'][0]}\n"
+        for i in data["instrument_names"]:
+            report_text += f"   {i[0]}: {i[1]}\n"
+        report_text += f"Используемых инструментов: {data['information'][2]}\n"
+        for i in data["users_instruments"]:
+            report_text += f"   Используется пользователем {i[0]} ({len(i[1])}): "
+            for j in i[1]:
+                report_text += f"{j}, \n"
+            if len(i[1])==0:
+                report_text+="-\n"
+        report_text += f"Новых инструментов: {data['information'][1]}\n"
+        report_text += f"Сломанных инструментов: {data['information'][3]}\n"
+        report_text += f"Инструментов в ремонте: {data['information'][4]}\n"
+        report_text += f"Дата генерации отчёта: {date.today()}"
+        report.write(report_text)
+        report.close()
+        return FileResponse(open("./report "+str(date.today())+".txt","rb"),as_attachment=True)
+    try:
+        os.remove("./report "+str(date.today())+".txt")
+    except:
+        pass
     return render(request, "inventory_report.html", context=data)
 def inventory_edit_instrument(request, id=1):
     error = False
@@ -80,12 +104,12 @@ def inventory_edit_instrument(request, id=1):
     cursor.execute('SELECT * FROM Inventory')
     inventory = [list(i) for i in cursor.fetchall()]
     cursor.execute('SELECT * FROM auth_user')
-    users_usernames = ["Ничьё"] + [i[4] for i in [list(i) for i in cursor.fetchall()]]
+    users_usernames = ["Свободно"] + [i[4] for i in [list(i) for i in cursor.fetchall()]]
     instrument = [i for i in inventory if i[0]==id][0]
     cursor.close()
     if request.method=="POST":
         connection, cursor = connect()
-        condition = "Новый" if request.POST.get("belonging", "") == "Ничьё" and request.POST.get("condition", "") == "Используемый" else "Используемый" if request.POST.get("belonging", "") != "Ничьё" and (request.POST.get("condition", "") in ["Новый", "Используемый"]) else request.POST.get("condition", "")
+        condition = "Новый" if request.POST.get("belonging", "") == "Свободно" and request.POST.get("condition", "") == "Используемый" else "Используемый" if request.POST.get("belonging", "") != "Свободно" and (request.POST.get("condition", "") in ["Новый", "Используемый"]) else request.POST.get("condition", "")
         if request.POST.get("name","") == "":
             error = True
         else:
@@ -99,3 +123,11 @@ def inventory_edit_instrument(request, id=1):
           "error": error,
           }
     return render(request, "inventory_edit_instrument.html", context=data)
+
+def inventory_delete_instrument(request, id=1):
+    connection, cursor = connect()
+    cursor.execute('DELETE FROM Inventory WHERE id = ?', (id, ))
+    cursor.execute('DELETE FROM applications WHERE instrument_id = ?', (id, ))
+    connection.commit()
+    cursor.close()
+    return redirect("inventory_editing")
